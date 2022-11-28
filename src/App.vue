3bee336c747edede9bf728c2c56f0f5bb4a676a9
yairamit <ysiraamit@gmail.com>
2022-11-28 16:47:53 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+ 
+<script>
+
+ import post from './components/post.vue';
+ export default{
+  name: 'App',
+    components:{
+      post,
+    },
+ }
+</script>
+
+
+<template>
+  <post />
+  
+ </template>
---
+++ src/components/post.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <form @submit.prevent="save">
+            <div>
+        <label for ="name"> product name</label>
+        <input type="text" id="name" v-model="formData.name">
+    </div>
+    <div>
+        <label for ="category"> category</label>
+        <input type="text" id="name" v-model="formData.category">
+    </div>
+    <div>
+        <label for ="price"> price</label>
+        <input type="text" id="price" v-model="formData.price">
+    </div>
+    <button> save</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { METHODS } from 'http';
+
+    export default{
+        name: 'post',
+        data(){
+            return{
+                name:'',
+                category:'',
+                price: ''
+
+            }
+        },
+       methods:{
+            save(){
+                axios.post('localhost:8004/products/',this.formData)
+                
+
+            }
+        }
+    }
+</script>
